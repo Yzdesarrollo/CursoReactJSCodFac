fc65a379511522313707bb061370a93ec5577505
yeison.ariel@accenture.com <yeison.ariel@accenture.com>
2020-06-13 17:55:22 -0500

    Formularios 1

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
 import React, { useState } from 'react';
 import { render } from 'react-dom';
 
-const Saludo = () =>{
-  const [name, setName ] = useState('');
+
+const Form = () =>{
+  let [title, setTitle] = useState("");
+  let [body, setBody] = useState("");
+
+const sendForm = (ev) => {
+  ev.preventDefault()
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+      title:title,
+      body: body,
+      userId: 1
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+  .then(response => response.json())
+  .then(json => console.log(json))
+}
+
   return(
+  <form onSubmit={(ev)=> sendForm(ev)}>
     <div>
-      {/* SyntheticEvent */}
-      <input type="text" onChange={ (ev) => setName(ev.target.value) }/>
-      <p>Hola {name} </p>
+      <label htmlFor="title">Título</label>
+      <input type="text" id="title" onChange={ (ev) => setTitle(ev.target.value) } />
     </div>
-  );
+     <div>
+      <label htmlFor="body">Publicación</label>
+      <textarea id="body" onChange={ (ev) => setBody(ev.target.value) }></textarea>
+    </div>
+    <input type="submit" value="Enviar" />
+  </form>
+  )
 }
 
 const App = () => {
-  return <div><Saludo /></div>
+  return <div> <Form /> </div>;
 }
 
 render(<App />, document.getElementById('titulo'));
