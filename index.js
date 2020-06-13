e752cdf11e2401a2cc95a4ffbf9789d2e2b925ac
yeison.ariel@accenture.com <yeison.ariel@accenture.com>
2020-06-13 15:26:23 -0500

    Creando un componente function

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const paises = ['Peru', 'Chile', 'Uruguay', 'Colombia']
-
-function getPaises()
-{
-  const elementos = [];
-  for(let i = 0; i < paises.length; i ++)
-  {
-    elementos.push(<li>{paises[i]}</li>);
-  }
-  return elementos;
-}
-
-const Paises = () => 
-{
-  return <ul>{ paises.map( (nombre, indice) => <li key={indice}>{nombre}</li> )}</ul>
-}
-
-const App = () =>
-{
-  return <h1><Paises /></h1>;
+function App() {
+  return <h1>Hola Mundo </h1>
 }
 
 render(<App />, document.getElementById('titulo'));
