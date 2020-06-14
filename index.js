8e6c8e277a50e06b56855f9f7a02ebf52811627c
yeison.ariel@accenture.com <yeison.ariel@accenture.com>
2020-06-13 19:46:24 -0500

    useState con Componentes Clase

--- index.js
+++ index.js
@@ -3,9 +3,23 @@ import { render } from 'react-dom';
 
 
 class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      contador: 0
+    }
+  }
+
   render(){
-    return <p>Hola mundo! </p>
+    console.log(this.props);
+    return(
+    <>
+    <p>Contador: {this.state.contador} </p>
+    <button onClick={ ()=>this.setState({ contador: this.state.contador + 1 }) }>Sumar</button>
+    </>
+    )
   }
 }
 
-render(<App />, document.getElementById('titulo'));
+render(<App name={2} />, document.getElementById('titulo'));
